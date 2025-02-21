3d5df01487777f01a3cb2383a7f745c2e9409b9d
maramravichandra <ravichandra.maram@gmail.com>
2025-02-20 22:51:09 -0500

    File and db operations

--- aws/iam_access.py
+++ aws/iam_access.py
@@ -1,10 +1,19 @@
 import boto3
 
 iam = boto3.resource("iam")
-
 def list_users():
     users = list(iam.users.all())
     print("All Users", str(users))
 
+ec2 = boto3.client("ec2")
+def list_ec2_instances():
+    instances = ec2.describe_instances()
+    print("Ec2 instances : ", str(list(instances)))
+    resvations = instances['Reservations']
+    print("Ec2 resvations instances : ", str(list(resvations)))
+    responseMetadata = instances['ResponseMetadata']
+    print("Ec2 responseMetadata instances : ", str(list(responseMetadata)))
+
 
 list_users()
+list_ec2_instances()
