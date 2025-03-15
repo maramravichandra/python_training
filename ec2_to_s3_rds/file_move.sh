a89075a61f25c11b232db03ec5b55da05a8f26ef
maramravichandra <ravichandra.maram@gmail.com>
2025-03-14 23:06:03 -0400

    EC2 to RDS

--- ec2_to_s3_rds/file_move.sh
+++ ec2_to_s3_rds/file_move.sh
@@ -6,7 +6,7 @@ for file in "${files[@]}"; do
     echo "Executing : aws s3 cp $files_dir/$file s3://image-upload-test-rc/user_files/$file"
     aws s3 cp $files_dir/$file s3://image-upload-test-rc/user_files/$file
     echo "File has been copied into S3 so deleting from EC2"
-    #rm $files_dir/$file
+    rm $files_dir/$file
 done
 
 echo "python3 ~/ec2_connect_rds.py testdb s3_object_details "${files[@]}" s3://image-upload-test-rc/user_files/ $(whoami)"
--- ec2_to_s3_rds/inital_setup.sh
+++ ec2_to_s3_rds/inital_setup.sh
@@ -1,5 +1,12 @@
 sudo yum list | grep python3
 python3 --version
 sudo yum install -y python3-pip python3 python3-setuptools
+echo "===>PIP install boto3"
 pip3 install boto3
+echo "===> PIP install mysql_connector_python"
+pip3 install mysql_connector_python
+mkdir ~/user_files
+echo "User files directory has been created."
+echo "Scheduling cron job for every 5 minutes"
+*/5 * * * * ~/file_move.sh
 
